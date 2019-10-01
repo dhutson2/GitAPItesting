0bf1f659eae1fc3a6b57388380e21fd30b80a9a7
dhutson2 <hutsond1301@gmail.com>
2019-10-01 14:34:29 +0900

    accessing user opening PR, open PRs

--- js/app.js
+++ js/app.js
@@ -5,29 +5,30 @@ const userInputValue = document.querySelector("#user");
 const repoInputValue = document.querySelector("#repo");
 const searchButton = document.querySelector(".searchButton");
 const nameContainer = document.querySelector(".main__profile-name");
-const unContainer = document.querySelector(".main__profile-username");
-const reposContainer = document.querySelector(".main__profile-repos");
-const urlContainer = document.querySelector(".main__profile-url");
+const commitsContainer = document.querySelector(".main__profile-commits");
+const reposContainer = document.querySelector(".main__profile-comments");
+const unContainer = document.querySelector(".main__profile-un");
 
 const client_id = "Iv1.de60148e0859c0ba";
 const client_secret = "0bda8d722351c069ed89dfc3ec874ea0f4e1770e";
 
 // change this to fetch repo
 const fetchUsers = async (user, repo) => {
-  const user_api_call = await fetch(`https://api.github.com/repos/${user}/${repo}/commits?client_id=${client_id}&
+  const api_call = await fetch(`https://api.github.com/repos/${user}/${repo}/pulls?client_id=${client_id}&
     client_secret=${client_secret}`);
 
-  const data = await user_api_call.json();
+  const data = await api_call.json();
   return { data };
 };
 
+// will return PR number which you can use to find commits/comments
 const showData = () => {
   fetchUsers(userInputValue.value, repoInputValue.value).then(res => {
     console.log(res, "<-- user data");
-    nameContainer.innerHTML = `Open pull requests: <span class="main__profile-value">${res.data.name}</span>`;
-    unContainer.innerHTML = `# of commits: <span class="main__profile-value">${res.data.login}</span>`;
+    nameContainer.innerHTML = `Open pull requests: <span class="main__profile-value">${res.data[0].title}</span>`;
+    commitsContainer.innerHTML = `# of commits: <span class="main__profile-value">${res.data[0].commits}</span>`;
     reposContainer.innerHTML = `Comments: <span class="main__profile-value">${res.data.public_repos}</span>`;
-    urlContainer.innerHTML = `Pull request Opened by: <span class="main__profile-value">${res.data.html_url}</span>`;
+    unContainer.innerHTML = `Pull request Opened by: <span class="main__profile-value">${res.data[3].user.login}</span>`;
   });
 };
 
