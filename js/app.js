7f5ba6a08594246d3e79d4e9fe351b77244c9b89
dhutson2 <hutsond1301@gmail.com>
2019-10-12 11:25:10 -0600

    still messing with logic...

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const unContainer = document.querySelector(".main__profile-un");
 
 const client_id = "Iv1.de60148e0859c0ba";
 const client_secret = "0bda8d722351c069ed89dfc3ec874ea0f4e1770e";
+const pullNumbers = [];
 
 // change this to fetch repo
 const fetchPullRequests = async (user, repo) => {
@@ -24,13 +25,22 @@ const fetchPullRequests = async (user, repo) => {
 // will return PR number which you can use to find commits/comments
 const showData = () => {
   fetchPullRequests(userInputValue.value, repoInputValue.value).then(res => {
-    const getRequests = () => {
-      res.data.forEach(function(pull) {
-        console.log(pull, "<-- foreach statement");
+    const getRequestNumbers = async () => {
+      await res.data.forEach(function(pull) {
+        pullNumbers.push(pull.number);
+        console.log(pullNumbers, "<-- foreach statement");
       });
     };
+    const fetchEachPull = async () => {
+      const api_call = await fetch(
+        `https://api.github.com/repos/${user}/${repo}/pulls/${
+          pullNumbers[4]
+        }?client_id=${client_id}&client_secret=${client_secret}`
+      );
+    };
     console.log(res.data, "<-- user data");
-    console.log(getRequests());
+    console.log(getRequestNumbers());
+    console.log(fetchEachPull(), "<-- each pull");
     // console.log(getComments(), "<--get comments as numbers");
     nameContainer.innerHTML = `Open pull requests: <span class="main__profile-value">${res.data[0].title}</span>`;
     commitsContainer.innerHTML = `# of commits: <span class="main__profile-value">${res.data[0].commits}</span>`;
